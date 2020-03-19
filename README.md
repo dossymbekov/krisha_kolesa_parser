e74f9668ae4b16ba6db26d2ab16f742cf864bfec
dossymbekov <berser91@gmail.com>
2020-03-19 14:53:44 +0600

    updated

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # krisha_kolesa_parser
 Parser for krisha and kolesa adds
 
+0. configure your settings.py file
 1. make ssh tunnel to server
 2. connect to postgres db
 3. push adds
---
+++ krisha/files/settings.py
@@ -0,0 +1,14 @@
+
+ssh_conf = {
+    'ssh_ip':'',
+    'ssh_port':'',
+    'ssh_user': '',
+    'ssh_private_key':'',
+    'remote_bind_address':'',
+    'remote_bind_address_port':'',
+
+    'ssh_db':'',
+    'ssh_db_user':'',
+    'ssh_db_pass':'',
+    'ssh_db_host':''
+}
